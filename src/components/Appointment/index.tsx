70f15c5b407fad5504068c9af300c2717bbe0770
matheusht <dev.matheustheodoro@gmail.com>
2024-04-14 21:42:50 -0300

    Services Component Finished

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -12,12 +12,32 @@ export function Appointment() {
           </h2>
           <form className="appointment__form flex flex-col gap-y-5">
             <div>
-              <div className="select relative flex items-center bg-pink-200">
-                <div className="">
+              <div className="select relative flex items-center rounded-lg border">
+                <div className="absolute right-4">
                   <i>
-                    <RiArrowDownSLine className="text-[26px]" />
+                    <RiArrowDownSLine className="text-[26px] text-primary" />
                   </i>
                 </div>
+                <select className="size-full appearance-none bg-transparent p-4 outline-none">
+                  <option value="1">Select department</option>
+                  <option value="2">Deparment 1</option>
+                  <option value="3">Deparment 2</option>
+                  <option value="4">Deparment 3</option>
+                </select>
+              </div>
+
+              <div className="select relative flex items-center rounded-lg border">
+                <div className="absolute right-4">
+                  <i>
+                    <RiArrowDownSLine className="text-[26px] text-primary" />
+                  </i>
+                </div>
+                <select className="size-full appearance-none bg-transparent p-4 outline-none">
+                  <option value="1">Select Doctor</option>
+                  <option value="2">Dr. Jane Doe</option>
+                  <option value="3">Dr. Jhoe Lindsey</option>
+                  <option value="4">Dr. Bob Smith</option>
+                </select>
               </div>
             </div>
           </form>
